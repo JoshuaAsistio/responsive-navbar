a925dcec589892ccba7d9c266c32e1b57c43a530
Elijah Joshua Asistio <113345134+JoshuaAsistio@users.noreply.github.com>
2022-10-24 20:52:30 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.getElementById('nav-toggle')
+    const navbar = document.getElementById('navbar')
+
+    navToggle.addEventListener('click', () => {
+        navbar.classList.toggle('transform')
+    })
+})
